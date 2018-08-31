70dbd39f8ae227c03d7b9555111ab8d166f2a416
tuandh <tuandh.ttud@gmail.com>
2018-08-31 18:02:46 +0700

    Your Message

---
+++ image_classification/ensemble_predict_submission.py
@@ -0,0 +1,16 @@
+import csv
+
+model_paths = ['E:\\PythonSource\\keras_practice\\image_classification\\voting\\01686.csv',
+               'E:\\PythonSource\\keras_practice\\image_classification\\voting\\01832.csv',
+               'E:\\PythonSource\\keras_practice\\image_classification\\voting\\01832_2.csv',
+               'E:\\PythonSource\\keras_practice\\image_classification\\voting\\001971.csv',
+               'E:\\PythonSource\\keras_practice\\image_classification\\voting\\02006.csv']
+
+weights =[0.98314, 0.98168, 0.98168, 0.98029, 0.98]
+
+num_models = 5
+
+dict = {}
+
+
+
